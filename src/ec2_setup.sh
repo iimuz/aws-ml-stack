1895164ba3751f0f1748c6f4d6552db56c76434f
iimuz <iz.primary.story+github@gmail.com>
2024-05-02 16:46:36 +0900

    :bug: chshが失敗するためログインシェルを強制的に書き換え

--- src/ec2_setup.sh
+++ src/ec2_setup.sh
@@ -39,15 +39,22 @@ ln -s /data/src
 # zshの設定
 sudo sh -c "echo $(which zsh) >> /etc/shells"
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> $HOME/.zshrc
-sudo chsh -s $(which zsh)
+# sudo chsh -s $(which zsh)
 # chshでデフォルトシェルを変えられなかった場合にpasswdを編集
-# sudo sed -i 's|ubuntu:/bin/bash|ubuntu:ubuntu:/home/linuxbrew/.linuxbrew/bin/bin/zsh|g' /etc/passwd
+sudo sed -i 's|ubuntu:/bin/bash|ubuntu:/home/linuxbrew/.linuxbrew/bin/zsh|g' /etc/passwd
 
 # 言語の設定
+readonly NODEJS_VERSION=20.12.2
 asdf plugin add nodejs
-asdf install nodejs 20.12.2
-asdf global nodejs 20.12.2
+asdf install nodejs $NODEJS_VERSION
+asdf global nodejs $NODEJS_VERSION
 
+readonly PYTHON_VERSION=3.11.9
 asdf plugin add python
-asdf install python 3.11.9
-asdf global python 3.11.9
+asdf install python $PYTHON_VERSION
+asdf global python $PYTHON_VERSION
+
+readonly JAVA_VERSION="openjdk.20.0.1"
+asdf plugin add java
+asdf install java $JAVA_VERSION
+asdf global java $JAVA_VERSION
